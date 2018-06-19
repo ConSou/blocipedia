629ec424b8cd76651dc98ff76d707e0be1458e27
Conor Souhrada <ConorSouhrada@gmail.com>
2018-06-19 10:48:02 -0600

    spec start

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
+  let(:wiki) {Wiki.create!(title: "Wiki title", body: "This is a wiki body", private: false)}
 
+  describe "attributes" do
+
+    it "has title, body and private attributes" do
+
+      expect(wiki).to have_attributes(title: "Wiki title", body: "This is a wiki body", private: false)
+    end
+  end
 end
